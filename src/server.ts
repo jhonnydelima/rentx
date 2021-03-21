6d50aac82e055e7b66b0bf8fbc1d6c95200c6a9c
Jhonny Lima <jlima@Jhonnys-MacBook-Pro.local>
2021-03-21 01:47:32 -0300

    refactoring structure

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,12 @@
 import express from "express";
 
-import { categoriesRoutes } from "./routes/categories.routes";
+import { router } from "./routes";
 
 const app = express();
 
 app.use(express.json());
 
-app.use("/categories", categoriesRoutes);
+app.use(router);
 
 app.listen(3333, () => {
   console.log("Server is running!");
